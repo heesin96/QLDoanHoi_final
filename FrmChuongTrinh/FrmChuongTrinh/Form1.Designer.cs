3e97921b467e3a31f34b8e2480c7fc17a5385f14
Hoai Trinh <1454050155trinh@ou.edu.vn>
2016-12-21 21:15:02 +0700

    Frm Chuong trinh da fix

---
+++ FrmChuongTrinh/FrmChuongTrinh/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace FrmChuongTrinh
+{
+    partial class FrmChuongTrinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvChuongTrinh = new System.Windows.Forms.DataGridView();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.btn_Huy = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChuongTrinh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvChuongTrinh
+            // 
+            this.dgvChuongTrinh.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvChuongTrinh.Location = new System.Drawing.Point(12, 12);
+            this.dgvChuongTrinh.Name = "dgvChuongTrinh";
+            this.dgvChuongTrinh.Size = new System.Drawing.Size(417, 222);
+            this.dgvChuongTrinh.TabIndex = 0;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(33, 255);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(119, 39);
+            this.btn_Save.TabIndex = 1;
+            this.btn_Save.Text = "Lưu";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // btn_Huy
+            // 
+            this.btn_Huy.Location = new System.Drawing.Point(268, 255);
+            this.btn_Huy.Name = "btn_Huy";
+            this.btn_Huy.Size = new System.Drawing.Size(124, 39);
+            this.btn_Huy.TabIndex = 1;
+            this.btn_Huy.Text = "Hủy";
+            this.btn_Huy.UseVisualStyleBackColor = true;
+            this.btn_Huy.Click += new System.EventHandler(this.btn_Huy_Click);
+            // 
+            // FrmChuongTrinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 315);
+            this.Controls.Add(this.btn_Huy);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.dgvChuongTrinh);
+            this.Name = "FrmChuongTrinh";
+            this.Text = "ChuongTrinh";
+            this.Load += new System.EventHandler(this.FrmChuongTrinh_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvChuongTrinh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvChuongTrinh;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Huy;
+    }
+}
+
---
+++ FrmChuongTrinh/FrmChuongTrinh/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace FrmChuongTrinh
+{
+    public partial class FrmChuongTrinh : Form
+    {
+        SqlConnection cn;
+        string cnStr;
+        DataSet ds;
+        DataTable ChuongTrinh;
+
+        public FrmChuongTrinh()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmChuongTrinh_Load(object sender, EventArgs e)
+        {
+            cnStr = "Server = .; Database = QLyDoanHoi; Integrated security = true";
+            cn = new SqlConnection(cnStr);
+
+            string sql = "SELECT * FROM ChuongTrinh";
+            dgvChuongTrinh.DataSource = GetChuongTrinhDataset().Tables[0];
+
+        }
+
+        public DataSet GetChuongTrinhDataset()
+        {
+            try
+            {
+                string sql = " SELECT * FROM ChuongTrinh";
+                SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+                ds = new DataSet();
+                da.Fill(ds);
+                return ds;
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter();
+            //insert
+            string ins = "INSERT INTO ChuongTrinh (MaChuongTrinh,TenCT,CapDoCT,ThoiGianBD) VALUES (@MaCT,@Ten,@CapDo,@ThoiGian)";
+            SqlCommand cmd = new SqlCommand(ins, cn);
+            cmd.Parameters.Add("@MaCT", SqlDbType.NChar, 10, "MaChuongTrinh");
+            cmd.Parameters.Add("@Ten", SqlDbType.NVarChar, 50, "TenCT");
+            cmd.Parameters.Add("@CapDo", SqlDbType.NVarChar, 50, "CapDoCT");
+            cmd.Parameters.Add("@ThoiGian", SqlDbType.NVarChar, 50, "ThoiGianBD");
+            da.InsertCommand = cmd;
+
+            //delete
+            string del = "DELETE FROM ChuongTrinh WHERE MaChuongTrinh = @MaCT";
+            SqlCommand cmd1 = new SqlCommand(del, cn);
+            cmd1.Parameters.Add("@MaCT", SqlDbType.NChar, 10, "MaChuongTrinh");
+            da.DeleteCommand = cmd1;
+
+            //update
+            string upd = "UPDATE ChuongTrinh SET TenCT = @Ten,CapDoCT = @CapDo,ThoiGianBD = @ThoiGian WHERE MaChuongTrinh = @MaCT";
+            SqlCommand cmd2 = new SqlCommand(upd, cn);
+            cmd2.Parameters.Add("@Ten", SqlDbType.NVarChar, 50, "TenCT");
+            cmd2.Parameters.Add("@CapDo", SqlDbType.NVarChar, 50, "CapDoCT");
+            cmd2.Parameters.Add("@ThoiGian", SqlDbType.NVarChar, 50, "ThoiGianBD");
+            da.UpdateCommand = cmd2;
+
+
+            da.Update(ds);
+        }
+
+        private void btn_Huy_Click(object sender, EventArgs e)
+        {
+            ds.RejectChanges();
+        }
+
+
+        
+
+    }
+}
